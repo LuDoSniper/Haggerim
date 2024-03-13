7cc5f843e4f05ce5ae05614fc6899072d1f3b9fd
Lucien Donnarieix <lucien.donnarieix@hesias.fr>
2024-03-13 11:01:44 +0100

    Ajout de boutons et textures

--- home.php
+++ home.php
@@ -14,8 +14,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Haggerim</title>
+    <link rel="stylesheet" href="style-home.css">
 </head>
 <body>
+    <div id="background">
+        <div class="blur"></div>
+    </div>
     
+    <header>
+        <img src="imgs/Haggerim_logo.png" alt="Le logo d'Haggerim">
+        <span><img src="imgs/user_modif.png" alt="Tête de Steeve dans Minecraft"></span>
+    </header>
+
+    <div id="body">
+        <div class="main-title">
+            <div class="noir"></div>
+            <h1>Bienvenue dans le monde d'<span>Haggerim</span> !</h1>
+        </div>
+
+        <div id="features">
+            <button>Rejoindre Haggerim</button>
+            <button>Gestion des royaumes</button>
+            <button disabled>Hall of Haggerim's beauty</button>
+            <button>Signaler un joueur</button>
+        </div>
+    </div>
 </body>
 </html>
